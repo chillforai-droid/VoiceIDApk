b019260dbccc5d9d0af773473426afdbe2f47ab2
Texly <chillforai@gmail.com>
2026-07-31 16:32:16 +0530

    Deploy via Texly Studio DevStudio

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file
 plugins {
     id("com.android.application") version "8.11.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
 }
 
 tasks.register("clean", Delete::class) {
